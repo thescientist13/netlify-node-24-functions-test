17855d8cae87f673872ab2a647731dc2a154ae15
Owen Buckley <owenbuckley13@gmail.com>
2025-12-13 21:21:12 -0500

    node version test

--- netlify/functions/version.mjs
+++ netlify/functions/version.mjs
@@ -1,5 +1,5 @@
 import { version } from 'node:process';
 
 export default async (req, context) => {
-  return new Response("testing URLPattern on Node " + version);
+  return new Response(`Node version is ${version}`);
 };
