336fdfc219b15d4bead80840006ae5e4b65b1689
Owen Buckley <owenbuckley13@gmail.com>
2025-12-13 21:13:53 -0500

    node version test

--- netlify/functions/test.mjs
+++ netlify/functions/test.mjs
@@ -1,5 +1,7 @@
+import { version } from 'node:process';
+
 export default async (req, context) => {
   const pattern = new URLPattern(req.url);
   console.log({ pattern });
-  return new Response("testing URLPattern");
+  return new Response("testing URLPattern on Node " + version);
 };
